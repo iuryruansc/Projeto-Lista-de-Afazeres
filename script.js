4b5681fca8a0616c2330ea3d3cb09ba4c15afa01
iuryruansc <iuryruansc@gmail.com>
2023-07-13 10:26:31 -0300

    resetando o input

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const list = document.querySelector('#lista-tarefas');
+let listArray = [];
 
-
+// função para adicionar itens na lista
 const addItem = () => {
-    const tarefa = document.querySelector('#texto-tarefa').value;
-    const newItem = document.createElement('li');
-    newItem.innerHTML = tarefa;
-    list.appendChild(newItem);
+    let tarefa = document.querySelector('#texto-tarefa').value;
+    if (tarefa != "") { 
+        const newItem = document.createElement('li');
+        newItem.innerHTML = tarefa;
+        list.appendChild(newItem);
+    } else {
+        alert("Insira uma tarefa válida")
+    }
+    document.getElementById("clear").reset();
+    listArray.push(tarefa);
+}
+
+// função para salvar os itens da lista
+const saveList = () => {
+    const listItems = localStorage.getItem('toDoList');
+    if (listItems === "") {
+        return
+    } else {
+        for (let index = 0; index < listItems.length; index += 1) {
+        }
+    }
 }
 
 const buttonAddItem = document.querySelector('#criar-tarefa');
 buttonAddItem.addEventListener('click', addItem);
+
+window.onload = () => {
+    
+}
